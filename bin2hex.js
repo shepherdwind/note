6284e239769cd9aefe0d5fecd5029e2e5fe182c2
Eward Song <eward.song@gmail.com>
2013-12-15 14:42:12 +0800

    Update bin2hex.js

--- bin2hex.js
+++ bin2hex.js
@@ -2,8 +2,11 @@
 // xxd生成的数据，会自动加上换行，比较麻烦，找了一圈，没有找到合适的
 // 代码，就自己用js写了，感觉fileSteam还是很适合的。
 var fs = require('fs');
-var steam = fs.createReadStream('./up/cmd.exe');
-var writefile = fs.createWriteStream('./up/cmd.hex');
+var file = './tools/PSTools/PsExec.exe';
+var steam = fs.createReadStream(file);
+var writefile = fs.createWriteStream(file.replace(/\.exe$/, '.hex'));
+
+writefile.write('0x');
 
 function writeTofile(write, buf){
   var code = buf[0];
@@ -16,7 +19,6 @@ function writeTofile(write, buf){
     i += 1;
     code = buf[i];
   }
-  // 一次性写入，如果每次写入一个code，会很慢很慢的
   write.write(str);
   console.log('writing....');
 }
